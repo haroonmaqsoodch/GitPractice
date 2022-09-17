216e575fe8da6bc2c7b8e5d093b261f664ae614f
Ammad Ahmed <iosammadahmed@gmail.com>
2022-09-17 14:19:29 -0400

    Edit Name

--- GitPractice/GitPractice/ViewController.swift
+++ GitPractice/GitPractice/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        // Ammad Ahmed
+        // Ammad Ahmed 2022
     }
 
 
