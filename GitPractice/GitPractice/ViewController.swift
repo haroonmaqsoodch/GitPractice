c7f06a3833f5a1efb47584c7decd6b7645013cff
Abdul Khan <>
2022-09-17 14:25:42 -0400

    Edit Name

--- GitPractice/GitPractice/ViewController.swift
+++ GitPractice/GitPractice/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        // Abdul Khan
-        // AK
+        //My name is Abdul Khan
     }
 
 
